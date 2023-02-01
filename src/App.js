e47d891efc91ce4b5c0b1d23d95a3be847d74bbf
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-02-01 15:49:53 -0600

    added a template form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react"
 import "./App.css"
 import UserForm from './components/form'
+import SignUp from "./components/template"
 
 function App() {
   return (
+    <div>
       <UserForm />
+      <SignUp />
+    </div>
   );
 }
 
