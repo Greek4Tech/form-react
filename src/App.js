29e60275de86322666bc715e7ddbf1180be36e72
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-02-02 11:28:04 -0600

    first take seems to work

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react"
 import "./App.css"
 import UserForm from './components/form'
 import SignUp from "./components/template"
+import ComboForm from "./components/comboForm"
+
 
 function App() {
   return (
     <div>
-      <UserForm />
-      <SignUp />
+      {/* <UserForm />
+      <SignUp /> */}
+      <ComboForm />
     </div>
   );
 }
--- src/components/comboForm.js
+++ src/components/comboForm.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -15,7 +15,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const theme = createTheme();
 
-const comboForm = () => {
+const ComboForm = () => {
     const [occupations, setOccupations] = useState([]);
     const [states, setStates] = useState([]);
     const [formData, setFormData] = useState({
@@ -70,9 +70,40 @@ useEffect(() => {
     <ThemeProvider theme={theme}>
         <Container component="main" maxWidth="xs">
         <CssBaseline />
-       
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              </Grid>
+          </Box>
+          </Box>
         </Container>
     </ThemeProvider>
   )
 
 }
+
+export default ComboForm
