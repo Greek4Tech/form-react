9fd6ebfe829543b614bcb0e4c641c7aa58f7f3da
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-02-15 17:20:03 -0600

    imported Browser Router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 // import '@fontsource/roboto/300.css';
 // import '@fontsource/roboto/400.css';
 // import '@fontsource/roboto/500.css';
