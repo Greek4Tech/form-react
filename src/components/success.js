0b81934cf84a9876d9a42db4fd9d7843bad4ae59
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-02-16 14:33:58 -0600

    landing page

--- src/components/success.js
+++ src/components/success.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Stack, Typography, Button } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
 
-
-const Success = () => {
-
+function Success() {
   return (
-    <h1>Hello World</h1>
+    <Stack
+      sx={{
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+      }}
+    >
+      <CheckCircle sx={{ fontSize: 80, color: 'success.main' }} />
+      <Typography variant="h4" sx={{ my: 4 }}>
+        Form submitted successfully!
+      </Typography>
+      <Button variant="contained" color="primary" href="/">
+        Back to Home
+      </Button>
+    </Stack>
   );
-};
+}
 
-export default Success;
+export default Success
